e9985e1e1d6e71fc1cbf855bbabc34cd83d1bfd8
IvanovAdrew <zyablik7771@gmail.com>
2019-04-09 23:46:21 +0200

    it don't work, now i don't understand why

---
+++ app/src/main/java/com/example/simpleprogram/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.simpleprogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int howMuchOreder;
+    int costOfCapuchino=34;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textViewOfTotalAmount = (TextView) findViewById(R.id.TotalAmount);
+                howMuchOreder+=costOfCapuchino;
+                textViewOfTotalAmount.setText(howMuchOreder);
+            }
+        });
+    }
+   /* public void ordering(View view){
+        TextView textViewOfTotalAmount = (TextView) findViewById(R.id.TotalAmount);
+        howMuchOreder+=costOfCapuchino;
+        textViewOfTotalAmount.setText(howMuchOreder);
+    }*/
+}
