bc82c92a94f53488a1ce6a22168ec8fa7b2d3446
IvanovAdrew <zyablik7771@gmail.com>
2019-04-21 11:22:56 +0200

    I try to take off with all this, unfortunately I did not work out

--- app/src/main/java/com/example/simpleprogram/NeArrayAdapter.java
+++ app/src/main/java/com/example/simpleprogram/NeArrayAdapter.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class NeArrayAdapter extends ArrayAdapter<Product> {
 
@@ -15,10 +18,33 @@ public class NeArrayAdapter extends ArrayAdapter<Product> {
     public View getView(int position, View convertView, ViewGroup parent){
         View v = convertView;
         LayoutInflater inflater = LayoutInflater.from(getContext());
+        ViewHolder vh;
         if (v == null) {
             v = inflater.inflate(R.layout.item_of_array,parent, false);
+            vh = new ViewHolder(v);
+            v.setTag(vh);
+        } else{
+            vh = (ViewHolder)v.getTag();
         }
-
+        Product newItem = getItem(position);
+        vh.image1.setImageResource(newItem.getPhotoId());
+        vh.name.setText(newItem.getName());
+        vh.price.setText(newItem.getCostOfProducts());
         return v;
     }
+}
+class ViewHolder{
+    TextView price;
+    TextView name;
+    Button button1;
+    ImageView image1;
+
+    public ViewHolder(View v) {
+        this.price = (TextView)v.findViewById(R.id.costOfCapuchino);
+        this.name = (TextView)v.findViewById(R.id.name);
+        //this.button1 = (TextView)v.findViewById(R.id.costOfCapuchino);
+        this.image1 = (ImageView) v.findViewById(R.id.cupOfSomething);
+    }
+
+
 }
